4c85c19a9bcae5653476126c43046c49e2c867d7
enzodaloia <112956500+enzodaloia@users.noreply.github.com>
2024-04-16 17:10:22 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,3 +6,12 @@ Console.WriteLine("**************");
 
 Console.WriteLine("Entrez 1 ou 2 pour jouer ou quittez.");
 int mode = Convert.ToInt32(Console.ReadLine());
+
+if(mode == 1)
+{
+    // Pour Jouer
+}
+else if(mode == 2)
+{
+    // Pour Quitter
+}
