5f96e50f1742ae1c0a6b1ce4509119cae8791b76
enzodaloia <112956500+enzodaloia@users.noreply.github.com>
2024-04-16 17:09:30 +0200

    Menu + Choix mode

--- Program.cs
+++ Program.cs
@@ -1,3 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("**************");
+Console.WriteLine("* 1) Jouer   *");
+Console.WriteLine("* 2) Quitter *");
+Console.WriteLine("**************");
 
+
+Console.WriteLine("Entrez 1 ou 2 pour jouer ou quittez.");
+int mode = Convert.ToInt32(Console.ReadLine());
